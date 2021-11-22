c07eb6af99c92262c13226354979b8487c482500
peterhlavatik <p.hlavatik@gmail.com>
2021-11-22 18:18:19 +0100

    Update DependencyContainer.swift

--- GoFitApp/Managers/DependencyContainer.swift
+++ GoFitApp/Managers/DependencyContainer.swift
@@ -9,14 +9,13 @@ import Foundation
 
 final class DependencyContainer {
     lazy var userDefaultsManager = UserDefaultsManager(self)
-//    lazy var credentialsManager = CredentialsManager(self)
+    lazy var credentialsManager = CredentialsManager(self)
     lazy var networkManager: NetworkManager = {
         let configuration = URLSessionConfiguration.default
         configuration.httpAdditionalHeaders = ["Content-Type": "application/json; charset=utf-8"]
         let networkManager = NetworkManager(self)
         return networkManager
     }()
-//
 //    lazy var logoutInterceptor = LogoutInterceptor(self)
 //    lazy var feedbackManager = FeedbackManager()
 //    lazy var coreDataStore = CoreDataStore(name: "CiceroniLens", in: .persistent)
