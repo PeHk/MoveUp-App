e8669c19bd8289242421c9aa3529b3a2fa9a87f2
peterhlavatik <p.hlavatik@gmail.com>
2021-12-06 13:40:04 +0100

    Update Helpers.swift

--- GoFitApp/Static/Helpers.swift
+++ GoFitApp/Static/Helpers.swift
@@ -12,7 +12,7 @@ struct Validators {
         if string.count > 100 {
             return false
         }
-        let emailFormat = "^[a-z0-9]+[\\._-]?[a-z0-9]+[@]\\w+[.]\\w{2,3}$"
+        let emailFormat = "^[a-z0-9]+[\\._]?[a-z0-9]+[@]\\w+[.]\\w{2,3}$"
         let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailFormat)
         return emailPredicate.evaluate(with: string)
     }
